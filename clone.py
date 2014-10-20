82fa3ec7ba3685412bc438741a23e6454070cc15
Ubuntu <ubuntu@ip-172-31-45-222.us-west-2.compute.internal>
2014-10-20 19:22:51 +0000

    Version with expect script

--- clone.py
+++ clone.py
@@ -7,24 +7,29 @@ import os
 NUMBER_OF_THREADS = 16
 URL = "https://bitbucket.org/"
 CLONE_DISK = "/media/sdb"
-CLONE_DIR = "/media/sdb/clones"
+CLONE_DIR = "/media/sdb/clones2"
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 count = 0;
 counter_lock = threading.Lock()
 rsync_lock = threading.Lock()
+disk_threshold = 2 # In percentage; 1 is for testing
 
 def check_space():
+    global disk_threshold
     while True:
         df = subprocess.check_output(['df', CLONE_DISK])
         df = df.split("\n")[1].split()
-        if int(df[4][0:-1]) > 80:
+        if int(df[4][0:-1]) > disk_threshold:
+            disk_threshold = 70
             with rsync_lock:
-                rsync = subprocess.Popen("rsync -ae 'ssh -p 2200' --delete " + CLONE_DIR + " mahmadza@da2.eecs.utk.edu:", shell=True)
+                rsync = subprocess.Popen("rsync -ae 'ssh -p 2200' --remove-source-files " + CLONE_DIR + " mahmadza@da2.eecs.utk.edu:", shell=True)
                 print rsync.communicate()
+                dir_removal = subprocess.Popen("rm -rf " + CLONE_DIR + "/*", shell=True)
+                dir_removal.communicate()
         time.sleep(20)
 
-def clone(start, step, repo_list):
+def clone(start, step, repo_list, exclusion):
     global count
     repo_len = len(repo_list)
     # For every repo available to this thread
@@ -33,6 +38,8 @@ def clone(start, step, repo_list):
         vct = repo_list[i][0]
         # Get repo's username/name pair
         fullname = repo_list[i][1]
+        if fullname.replace("/", "~") in exclusion:
+            continue
         # Determine versioning system and run cloning in silent mode
         if vct == 'hg':
             process_command = ['hg', 'clone', '-U', URL + fullname, fullname.replace("/", "~"), '-q']
@@ -42,7 +49,7 @@ def clone(start, step, repo_list):
             # Run a subprocess that clones, if we're not running rsync
             while rsync_lock.locked():
                 time.sleep(0.1)
-            cloning_agent = subprocess.Popen(process_command)
+            cloning_agent = subprocess.Popen(process_command, stdin=subprocess.PIPE)
             cloning_agent.communicate()
             with counter_lock:
                 count+=1
@@ -52,6 +59,9 @@ def clone(start, step, repo_list):
         print "Number of repos cloned: %d\r"%count,
 
 if __name__ == "__main__":
+    # Exclude the ones already fetched
+    exclusion = open('repos.txt', 'r').readlines()
+    exclusion = [item.rstrip() for item in exclusion]
     # Make a directory to put everything inside
     try:
         os.mkdir(CLONE_DIR)
@@ -65,7 +75,7 @@ if __name__ == "__main__":
     for thread_number in range(NUMBER_OF_THREADS - 1):
         # Divide the list and start Daemon threads
         step = len(repo_list) / NUMBER_OF_THREADS
-        t = threading.Thread(target=clone, args=(thread_number*step, step, repo_list))
+        t = threading.Thread(target=clone, args=(thread_number*step, step, repo_list, exclusion))
         t.setDaemon(True)
         t.start()
         threads.append(t)
